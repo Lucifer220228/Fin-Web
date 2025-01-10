93684c55c5312032f40eecc6b43e1c34bfa848e2
Lucifer220228 <samy25502812@gmail.com>
2025-01-10 22:15:23 +0800

    react app.tsx

--- react/src/view/App.tsx
+++ react/src/view/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import OrdersPage from '../pages/OrdersPage';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <nav>
+        <Link to="/">首頁</Link> | <Link to="/orders">訂單管理</Link>
+      </nav>
       <Routes>
         {/* 首頁 */}
         <Route path="/" element={<HomePage />} />
